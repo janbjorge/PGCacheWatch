e7f9d7431851b88067246b511d2090bd71a5f7a5
JB Lovland <janbjorge@gmail.com>
2024-02-26 09:11:31 +0100

    Remove html logo

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,3 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_static_path = ["_static"]
 pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
-html_logo = "logo.webp"
-html_theme_options = {
-    "style_nav_header_background": "#C0C0C0",
-}
