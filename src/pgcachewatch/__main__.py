bf48570edba96bb0e87c555dd22f82e6eeb57939
janbjorge <janbjorge@gmail.com>
2024-04-11 13:27:08 +0200

    Remove signal handling in main execution (#23)

--- src/pgcachewatch/__main__.py
+++ src/pgcachewatch/__main__.py
@@ -1,10 +1,6 @@
 import asyncio
-import contextlib
-import signal
 
 from pgcachewatch import cli
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-    with contextlib.suppress(KeyboardInterrupt):
-        asyncio.run(cli.main())
+    asyncio.run(cli.main())
